9db4830cc7aeec4aef9ecd8390d889be2074fed1
Changbeom Ha <hatcha82@gmail.com>
2018-11-19 21:43:52 +0900

    ok test

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,9 @@
       md6
       lg4>
       <div class="text-xs-center">            
-        <music-list ref="randomMusic" :songs="randomMusicList1"/>
+        <music-list 
+          ref="randomMusic" 
+          :songs="randomSongs"/>
       </div>      
     </v-flex>
     <v-flex
@@ -25,7 +27,9 @@
       md6
       lg4>
       <div class="text-xs-center">    
-         <music-list ref="randomMusic2" :songs="randomMusicList2"/>
+        <music-list 
+          ref="randomMusic2" 
+          :songs="randomSongs"/>
       </div>      
     </v-flex>
     <v-flex
@@ -34,7 +38,9 @@
       md6
       lg4>
       <div class="text-xs-center">    
-         <music-list ref="randomMusic3" :songs="randomMusicList3"/>
+        <music-list 
+          ref="randomMusic3" 
+          :songs="randomSongs"/>
       </div>      
     </v-flex>
   </v-layout>
@@ -44,19 +50,18 @@
 import FuriganaLogo from '~/components/FuriganaLogo.vue'
 import MusicList from '~/components/music/MusicList.vue'
 export default {
-  components: {    
+  components: {
     FuriganaLogo,
     MusicList
   },
-  async asyncData({ app }) {    
-      let randomMusicList1 = (await app.$axios.$get('/songs/randomeSong', { progress: true })).data
-      let randomMusicList2 = (await app.$axios.$get('/songs/randomeSong', { progress: true })).data
-      let randomMusicList3 = (await app.$axios.$get('/songs/randomeSong', { progress: true })).data
-    
-    return { 
-      randomMusicList1,
-      randomMusicList2, 
-      randomMusicList3,
+  async asyncData({ app }) {
+    let { randomSongs, recenstNews } = await app.$axios.$get('/home', {
+      progress: true
+    })
+
+    return {
+      randomSongs,
+      recenstNews
     }
   }
 }
