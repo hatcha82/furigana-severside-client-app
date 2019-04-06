07ff0a43f717d472c01799facdb432a84850c09f
Changbeom Ha <hatcha82@gmail.com>
2019-04-06 20:50:05 +0900

    e

--- components/adv/AdComponent.vue
+++ components/adv/AdComponent.vue
@@ -5,21 +5,6 @@
     <v-flex
       xs12
       sm12
-      md6 
-      lg6
-      class="pt-1 pb-1"
-    >
-      <iframe 
-        width="728" 
-        height="90" 
-        allowtransparency="true" 
-        src="https://tab2.clickmon.co.kr/pop/wp_ad_728.php?PopAd=CM_M_1003067%7C%5E%7CCM_A_1054519%7C%5E%7CAdver_M_1046207&mon_rf=REFERRER_URL" 
-        frameborder="0" 
-        scrolling="no"/>
-    </v-flex>
-    <v-flex
-      xs12
-      sm12
       md6
       lg6
       class="pt-1 pb-1"
@@ -36,6 +21,34 @@
         data-ad-test="N"/>
      
     </v-flex>
+    <v-flex
+      xs12
+      sm12
+      md6 
+      lg6
+      class="pt-1 pb-1"
+    >
+      <div style="margin:auto"> 
+        <iframe 
+          width="728%" 
+          height="90" 
+          allowtransparency="true" 
+          src="https://tab2.clickmon.co.kr/pop/wp_ad_728.php?PopAd=CM_M_1003067%7C%5E%7CCM_A_1054519%7C%5E%7CAdver_M_1046207&mon_rf=REFERRER_URL" 
+          frameborder="0" 
+    
+          class="hidden-sm-and-down"
+          scrolling="no"/>
+        <iframe 
+          width="320" 
+          height="100" 
+          allowtransparency="true" 
+          src="https://mtab.clickmon.co.kr/pop/wp_m_320_100.php?PopAd=CM_M_1003067%7C%5E%7CCM_A_1054519%7C%5E%7CAdver_M_1046207&mon_rf=REFERRER_URL" 
+          class="display-sm-and-up hidden-md-and-up"
+          frameborder="0" 
+          scrolling="no"/>
+      </div>
+    </v-flex>
+
   </v-layout>
  
 </template>
